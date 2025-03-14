f85f7f6b11b0220438ee6040a537262235069a2b
SeryjAlexey <alekseyvs.2005@gmail.com>
2025-03-14 18:00:10 +0300

    почистил код

--- Assets/Scripts/MovableObject.cs
+++ Assets/Scripts/MovableObject.cs
@@ -3,18 +3,16 @@ using UnityEngine;
 public class MovableObject : MonoBehaviour, IInteractable
 {
     private Rigidbody2D rb;
-    [SerializeField]private Collider2D collider2d;
-    private Sprite sprite;
+    [SerializeField]private Collider2D lower_collider;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        sprite = GetComponent<SpriteRenderer>().sprite;
     }
     public void Interact(Touch touch)
     {
         if(touch.phase == TouchPhase.Began)
         {
-            collider2d.isTrigger = true;
+            lower_collider.isTrigger = true;
             rb.bodyType = RigidbodyType2D.Static;
         }
         else if (touch.phase == TouchPhase.Moved)
@@ -24,9 +22,9 @@ public class MovableObject : MonoBehaviour, IInteractable
         }
         else if(touch.phase == TouchPhase.Ended)
         {
-            if(Physics2D.OverlapAreaAll(collider2d.bounds.min, collider2d.bounds.max).Length <= 2)
+            if(Physics2D.OverlapAreaAll(lower_collider.bounds.min, lower_collider.bounds.max).Length <= 2)
             {
-              collider2d.isTrigger = false;
+              lower_collider.isTrigger = false;
               rb.bodyType = RigidbodyType2D.Dynamic;
             }
         }
